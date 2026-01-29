61f2adcb449b183e0bce5ac28b88192ac2b61fb8
dereksuun <deryck.ferreira30@gmail.com>
2026-01-29 15:50:29 -0300

    ##3##

--- arqia_-2/arqia/urls.py
+++ arqia_-2/arqia/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from analise.views import healthcheck
 
 urlpatterns = [
+    path('', healthcheck, name='root-healthcheck'),
     path('admin/', admin.site.urls),         # Acesso ao admin
     path('api/', include('analise.urls')),   # Todas as rotas da app analise
 ]
